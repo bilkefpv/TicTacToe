3006872a52ef1dbf1da55edadb452b24a1b53066
bilkefpv <bilke1104@gmail.com>
2024-10-06 20:09:12 +0200

    add req

--- start.py
+++ start.py
@@ -6,7 +6,7 @@ from agent import snake_agent
 window = tk.Tk()
 
 window.title("Bilke Window")
-window.geometry('350x200')
+window.geometry('450x200')  # Adjusted for more space
 lbl = tk.Label(window, text="Select a Game")
 lbl.grid(column=0, row=0)
 
@@ -19,13 +19,19 @@ def tictacstart():
 def pong_start():
     pong.start()
 
-btn = tk.Button(window, text="Tic Tac Toe", command=tictacstart)
-btn.grid(column=0, row=1)
-btn = tk.Button(window, text="Pong", command=pong_start)
-btn.grid(column=1, row=1)
-btn = tk.Button(window, text="Snake", command=snakestart)
-btn.grid(column=2, row=1)
-btn = tk.Button(window, text="Snake Continue Learning", command=snakeContinue)
-btn.grid(column=3, row=1)
+
+btn_ttt = tk.Button(window, text="Tic Tac Toe (Normal game)", command=tictacstart)
+btn_ttt.grid(column=0, row=1)
+
+btn_pong = tk.Button(window, text="Pong (Vs player or NPC)", command=pong_start)
+btn_pong.grid(column=1, row=1)
+
+btn_snake = tk.Button(window, text="Snake (Normal game)", command=snakestart)
+btn_snake.grid(column=2, row=1)
+
+btn_snake_ai = tk.Button(window, text="Snake AI (Continue Learning)", command=snakeContinue, bg="yellow", fg="black", font=("Arial", 12, "bold"))
+
+btn_snake_ai.grid(column=0, row=2, columnspan=4, pady=20) 
+
 window.mainloop()
 
